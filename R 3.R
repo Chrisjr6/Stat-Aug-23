a65cf57f6d3f23d36cf5ab23d115650bb5993498
Chrisjr6 <89663353+Chrisjr6@users.noreply.github.com>
2021-08-31 11:46:06 -0400

    Add files via upload

---
+++ R 3.R
@@ -0,0 +1,11 @@
+# Install a package called "ggpubr"
+
+if(!require(devtools)) install.packages("devtools")
+
+devtools::install_github("kassambara/ggpubr")
+
+# Load library ggpubr
+
+library("ggpubr")
+
+# Load some sample dataset
